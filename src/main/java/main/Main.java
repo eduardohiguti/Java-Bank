ac72d28f985cd60c32deb046e124833b54785347
Eduardo Higuti <eduardohiguti@outlook.com>
2024-06-15 11:21:15 -0300

    Teste

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import entities.user.*;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new Manager(1, "Carlinhos", "gerente123");
+    }
+}
